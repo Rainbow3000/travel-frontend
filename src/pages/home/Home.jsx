7568d35e78b8954fadf4d8dd4faac7251533b864
Nguyen Duc Thinh <nguyenducthinh0401@gmail.com>
2023-03-30 23:09:05 +0700

    update-travel-frontend

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react'
+import './home.scss'
+
+const Home = () => {    
+  return (
+    <div className='home-container'>
+        Home
+    </div>
+  )
+}
+
+export default Home
