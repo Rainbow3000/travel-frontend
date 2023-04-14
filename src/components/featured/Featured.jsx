83b248c04af2ac2970862025419a07854af468dd
Nguyen Duc Thinh <nguyenducthinh0401@gmail.com>
2023-04-14 11:54:40 +0700

    frontend update

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './featured.scss'
-import featured from '../../images/featured.jpg'
+import featured from './featured.jpg'
 const Featured = () => {
   return (
     <div className='featured-container'>
-        <img src={featured} alt="" />
+        <img src={featured} alt="featured img" />
         <div className="featured-introduct">
             <span> <span style={{color:"blue"}} >LINK TRAVEL!</span> Chúng tôi cung cấp cho bạn một trải nghiệm du lịch tuyệt vời với những chuyến đi thú vị, các địa điểm du lịch đẹp mắt và các hoạt động giải trí đa dạng.Rất hân hạnh khi được đồng hành cùng với bạn !</span>
             <button>        
