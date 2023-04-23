30ad3301ee5eb717b8ffc71fee40d4f59a43c79c
Nguyen Duc Thinh <nguyenducthinh0401@gmail.com>
2023-04-23 22:30:47 +0700

    redeploy-frontend

--- src/requestMethod.js
+++ src/requestMethod.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "https://proud-time-production.up.railway.app/api/v1"
+const baseURL = "https://travel-backend-production-ea7f.up.railway.app/api/v1"
  
 
 const publicRequest = axios.create({
